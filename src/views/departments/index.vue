16af7f017cc1056ec3e943fbf34d97e0a875e8ac
renbuairen <1664844124@qq.com>
2022-08-05 11:35:05 +0800

    完成树形结构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <!-- 头部 -->
+        <TreeTools :isRoot="true" :treeNode="company" />
+        <!-- 树形 -->
+        <el-tree :data="treeData" :props="defaultProps" default-expand-all>
+          <template v-slot="{ data }">
+            <TreeTools :treeNode="data" />
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
 export default {
   data() {
-    return {}
+    return {
+      treeData: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' }
+      ],
+      defaultProps: {
+        label: 'name' // 将data中哪个数据名显示到树形页面中
+      },
+      company: { name: '传智教育', manager: '负责人' }
+    }
+  },
+  components: {
+    TreeTools
   },
 
   created() {},
